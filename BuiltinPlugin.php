92f5c4c34c3ff272d2b478fe38832aa261715dd1
teleclient <54826170+teleclient@users.noreply.github.com>
2021-02-04 00:10:10 -0800

    Finish adding empty plugins!

--- BuiltinPlugin.php
+++ BuiltinPlugin.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 class BuiltinPlugin
 {
-    private EventHandler $api;
+    private EventHandler $eh;
 
-    function __construct(EventHandler $api)
+    function __construct(EventHandler $eh)
     {
-        $this->api = $api;
+        $this->eh = $eh;
     }
 
     public function onStart(): \Generator
@@ -19,26 +19,7 @@ class BuiltinPlugin
 
     public function __invoke(array $update): \Generator
     {
-
-        if (false && isset($update['message']['media']) && $update['message']['media']['_'] !== 'messageMediaGame') {
-            yield $this->downloadToDir($update, '/tmp');
-            yield $this->messages->sendMedia([
-                'peer' => $update,
-                'message' => $update['message']['message'],
-                'media' => $update
-            ]);
-        }
-
-        $res = json_encode($update, JSON_PRETTY_PRINT);
-
-        yield $this->sleep(3);
-
-        try {
-            yield $this->sm($update, "<code>$res</code>\nAsynchronously, after 3 seconds");
-        } catch (\danog\MadelineProto\RPCErrorException $e) {
-            \danog\MadelineProto\Logger::log((string) $e, \danog\MadelineProto\Logger::FATAL_ERROR);
-        } catch (\danog\MadelineProto\Exception $e) {
-            \danog\MadelineProto\Logger::log((string) $e, \danog\MadelineProto\Logger::FATAL_ERROR);
-        }
+        return;
+        yield;
     }
 }
